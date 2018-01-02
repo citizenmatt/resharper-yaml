45a5eb43c5ee429cde81670c1ff036c92fbae776
Matt Ellis <m.t.ellis@gmail.com>
2018-01-02 12:36:58 +0000

    Initial commit
    Skeleton YAML project and PSI registration, plus a ton of lexer tests, currently unimplemented

---
+++ resharper/src/resharper-yaml/Psi/Parsing/YamlTokenType.Instances.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.ReSharper.Psi.Parsing;
+
+namespace JetBrains.ReSharper.Plugins.Yaml.Psi.Parsing
+{
+  public static partial class YamlTokenType
+  {
+    public static readonly TokenNodeType BAD_CHARACTER = new GenericTokenNodeType("BAD_CHARACTER", LAST_GENERATED_TOKEN_TYPE_INDEX + 10, "�");
+
+    public static readonly TokenNodeType EOF = new GenericTokenNodeType("EOF", LAST_GENERATED_TOKEN_TYPE_INDEX + 11, "EOF");
+
+    public static readonly TokenNodeType NEW_LINE = new NewLineNodeType(LAST_GENERATED_TOKEN_TYPE_INDEX + 12);
+    public static readonly TokenNodeType WHITESPACE = new WhitespaceNodeType(LAST_GENERATED_TOKEN_TYPE_INDEX + 13);
+  }
+}
