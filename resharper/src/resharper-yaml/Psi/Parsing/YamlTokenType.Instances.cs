58c5e9df4b2f86d1965bd1bf831ed30dfc9a9aba
Matt Ellis <m.t.ellis@gmail.com>
2018-01-03 11:23:56 +0000

    Lex single line, single quoted text

--- resharper/src/resharper-yaml/Psi/Parsing/YamlTokenType.Instances.cs
+++ resharper/src/resharper-yaml/Psi/Parsing/YamlTokenType.Instances.cs
@@ -13,9 +13,10 @@ namespace JetBrains.ReSharper.Plugins.Yaml.Psi.Parsing
 
     public static readonly TokenNodeType COMMENT = new CommentTokenNodeType(LAST_GENERATED_TOKEN_TYPE_INDEX + 14);
 
-    // TODO: naming...
+    // TODO: Naming. The YAML spec has an interesting hungarian notation style...
     public static readonly TokenNodeType IDENTIFIER = new GenericTokenNodeType("IDENTIFIER", LAST_GENERATED_TOKEN_TYPE_INDEX + 20, "IDENTIFIER");
     public static readonly TokenNodeType NS_PLAIN = new GenericTokenNodeType("NS_PLAIN", LAST_GENERATED_TOKEN_TYPE_INDEX + 21, "NS_PLAIN");
     public static readonly TokenNodeType NS_ANCHOR_NAME = new GenericTokenNodeType("NS_ANCHOR_NAME", LAST_GENERATED_TOKEN_TYPE_INDEX + 22, "NS_ANCHOR_NAME");
+    public static readonly TokenNodeType C_SINGLE_QUOTED = new GenericTokenNodeType("C_SINGLE_QUOTED", LAST_GENERATED_TOKEN_TYPE_INDEX + 23, "C_SINGLE_QUOTED");
   }
 }
