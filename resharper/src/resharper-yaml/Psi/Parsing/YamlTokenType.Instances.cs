a9a3058e88de0b7ff1d8bc51786c3f6f98f785ce
Matt Ellis <m.t.ellis@gmail.com>
2018-01-03 11:52:26 +0000

    Lex single line, double quoted text

--- resharper/src/resharper-yaml/Psi/Parsing/YamlTokenType.Instances.cs
+++ resharper/src/resharper-yaml/Psi/Parsing/YamlTokenType.Instances.cs
@@ -18,5 +18,6 @@ namespace JetBrains.ReSharper.Plugins.Yaml.Psi.Parsing
     public static readonly TokenNodeType NS_PLAIN = new GenericTokenNodeType("NS_PLAIN", LAST_GENERATED_TOKEN_TYPE_INDEX + 21, "NS_PLAIN");
     public static readonly TokenNodeType NS_ANCHOR_NAME = new GenericTokenNodeType("NS_ANCHOR_NAME", LAST_GENERATED_TOKEN_TYPE_INDEX + 22, "NS_ANCHOR_NAME");
     public static readonly TokenNodeType C_SINGLE_QUOTED = new GenericTokenNodeType("C_SINGLE_QUOTED", LAST_GENERATED_TOKEN_TYPE_INDEX + 23, "C_SINGLE_QUOTED");
+    public static readonly TokenNodeType C_DOUBLE_QUOTED = new GenericTokenNodeType("C_DOUBLE_QUOTED", LAST_GENERATED_TOKEN_TYPE_INDEX + 24, "C_DOUBLE_QUOTED");
   }
 }
